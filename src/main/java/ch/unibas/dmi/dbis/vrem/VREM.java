530c32bb4a79f654d51e4df04bb249f096742bab
Ralph Gasser <ralph.gasser@unibas.ch>
2018-10-27 17:21:59 +0200

    Cleanup

--- src/main/java/ch/unibas/dmi/dbis/vrem/VREM.java
+++ src/main/java/ch/unibas/dmi/dbis/vrem/VREM.java
@@ -4,24 +4,8 @@ import ch.unibas.dmi.dbis.vrem.importer.DemoImporter;
 import ch.unibas.dmi.dbis.vrem.server.WebServer;
 import com.github.rvesse.airline.annotations.Cli;
 
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @Cli(name = "VREM", description = "Virtual Reality Exhibition Manager", commands = {WebServer.class, DemoImporter.class}, defaultCommand = WebServer.class)
 public class VREM {
-    /** */
-    private static final String MONGO_HOST = "127.0.0.1";
-
-    /** */
-    private static final int MONGO_PORT = 27017;
-
-    /** */
-    private static final String MONGO_DATABASE = "vrem";
-
-    /** */
-    private static final Path DOCUMENT_ROOT = Paths.get("/Users/gassra02/Downloads/vrem");
-
     /**
      *
      * @param args
