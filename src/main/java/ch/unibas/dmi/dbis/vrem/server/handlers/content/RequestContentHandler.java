860d22969fbfc292cadac5bb847b0ac6f5d90608
Silvan Heller <silvan.heller@unibas.ch>
2019-04-12 15:51:51 +0200

    adding debug for 404 paths

--- src/main/java/ch/unibas/dmi/dbis/vrem/server/handlers/content/RequestContentHandler.java
+++ src/main/java/ch/unibas/dmi/dbis/vrem/server/handlers/content/RequestContentHandler.java
@@ -1,34 +1,30 @@
 package ch.unibas.dmi.dbis.vrem.server.handlers.content;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.WritableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 public class RequestContentHandler implements Route {
 
 
     private final Path root;
+    private Logger LOGGER = LogManager.getLogger();
 
     /**
      *
-     * @param root
      */
     public RequestContentHandler(Path root) {
         this.root = root;
     }
-
 
 
     @Override
@@ -38,13 +34,14 @@ public class RequestContentHandler implements Route {
 
         final String path = params.get(":path");
 
-        if (path == null){
+        if (path == null) {
             response.status(404);
             return 404;
         }
 
         final Path absolute = this.root.resolve(path);
         if (!Files.exists(absolute)) {
+            LOGGER.debug("Path {} does not exist", absolute.toAbsolutePath().toString());
             response.status(404);
             return 404;
         }
@@ -53,8 +50,7 @@ public class RequestContentHandler implements Route {
         response.type(Files.probeContentType(absolute));
         response.header("Transfer-Encoding", "identity");
         /* Transfer data. */
-        try (final InputStream in =  Files.newInputStream(absolute, StandardOpenOption.READ);
-             final OutputStream out = response.raw().getOutputStream()) {
+        try (final InputStream in = Files.newInputStream(absolute, StandardOpenOption.READ); final OutputStream out = response.raw().getOutputStream()) {
             int length = fastCopy(in, out);
             response.header("Content-Length", String.valueOf(length));
             out.flush();
@@ -65,16 +61,12 @@ public class RequestContentHandler implements Route {
 
     /**
      *
-     * @param src
-     * @param dest
-     * @throws IOException
      */
     public static int fastCopy(final InputStream src, final OutputStream dest) throws IOException {
         byte[] buffer = new byte[1024]; // Adjust if you want
         int bytesRead = 0;
         int totalBytesRead = 0;
-        while ((bytesRead = src.read(buffer)) != -1)
-        {
+        while ((bytesRead = src.read(buffer)) != -1) {
             dest.write(buffer, 0, bytesRead);
             totalBytesRead += bytesRead;
         }
